8099655c51eff238fa18f1129a6d1552ea71ad17
Manasés Jesús <contact@manasesjesus.com>
2019-12-11 15:23:45 +0100

    adding application draft

--- src/app.js
+++ src/app.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const pdf = require("html-pdf");
+const requestify = require("requestify");
+
+var externalURL= "http://...";
+
+
+requestify.get(externalURL).then(function (response) {
+    // Get the raw HTML response body
+    let html = response.body; 
+    let config = {
+        format: "A4", 
+        base: "https://cumulocity.com"
+    };
+
+    // Create the PDF
+    pdf.create(html, config).toFile('generated-pdf/generated.pdf', function (err, res) {
+      if (err) return console.log(err);
+      console.log(res);     // { filename: '...' }
+   });
+});
